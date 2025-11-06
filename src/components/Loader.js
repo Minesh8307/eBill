d788798687f68e8f3a7b43a49e26e397d42b7e0e
Minesh8307 <minesh.gla_cs23@gla.ac.in>
2025-11-06 21:13:35 +0530

    Add Loader component for loading indication

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="loader">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default Loader;
 
