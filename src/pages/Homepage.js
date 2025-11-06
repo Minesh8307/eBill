94ce4ece3dfaf22e49deb4dfd8af959e52410102
Minesh8307 <minesh.gla_cs23@gla.ac.in>
2025-11-06 23:15:51 +0530

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Homepage() {
+export default function Homepage() {
   return (
-    <div className="homepage">
-      <div className="intro-card glass">
-        <h1>Welcome to <span className="highlight">eBILL</span></h1>
-        <p>Smart online billing and inventory management made simple.</p>
+    <div className="homepage glass">
+      <h1>Welcome to <span className="highlight">eBILL</span></h1>
+      <p>Smart online billing and inventory management.</p>
+      <div className="home-buttons">
         <Link to="/login" className="btn-primary">Login</Link>
+        <Link to="/signup" className="btn-green">Sign Up</Link>
       </div>
     </div>
   );
 }
-
-export default Homepage;
 
