91cd4d09b091685098d5330c0f387a605d862831
Minesh8307 <minesh.gla_cs23@gla.ac.in>
2025-11-06 21:14:15 +0530

    Implement AuthContext for user authentication

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,1 +1,29 @@
+import React, { useContext, useState, useEffect, createContext } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../config/firebaseConfig";
 
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  function logout() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = { currentUser, logout };
+  return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
+}
