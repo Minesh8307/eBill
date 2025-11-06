bf9506e4aa8746b3ddfcf2f6f4ea94b88c55b4c5
Minesh8307 <minesh.gla_cs23@gla.ac.in>
2025-11-06 21:30:10 +0530

    Update salesService.js

--- src/services/salesService.js
+++ src/services/salesService.js
@@ -1,1 +1,11 @@
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { db } from "../config/firebaseConfig";
+
+const salesRef = collection(db, "sales");
+
+export const addSale = async (sale) => await addDoc(salesRef, sale);
+export const getSales = async () => {
+  const snapshot = await getDocs(salesRef);
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
 
