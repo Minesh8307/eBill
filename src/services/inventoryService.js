583b25625f4a1c7c2269c02d4b7ecc19d146ddcc
Minesh8307 <minesh.gla_cs23@gla.ac.in>
2025-11-06 21:29:47 +0530

    Update inventoryService.js

--- src/services/inventoryService.js
+++ src/services/inventoryService.js
@@ -1,1 +1,13 @@
+import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { db } from "../config/firebaseConfig";
+
+const inventoryRef = collection(db, "inventory");
+
+export const addItem = async (item) => await addDoc(inventoryRef, item);
+export const getItems = async () => {
+  const snapshot = await getDocs(inventoryRef);
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
+export const updateItem = async (id, updatedData) => await updateDoc(doc(db, "inventory", id), updatedData);
+export const deleteItem = async (id) => await deleteDoc(doc(db, "inventory", id));
 
